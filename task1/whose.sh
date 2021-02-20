08087f5721b5a4d49eaa6100c258a4452e379322
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-20 12:15:56 +0300

    Parameter checking for process's name or PID was added.

--- task1/whose.sh
+++ task1/whose.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-PROCESS=$1
+if [ ! -n "$1" ]
+ then 
+ 	echo "Process name or PID must be specified";
+ 	echo "Example of usage: 'whose.sh firefox' or 'whose.sh 1287'";
+ 	exit 1;
+fi 	
+  PROCESS=$1
 sudo netstat -tunapl | awk -v pat="$PROCESS" '$7~pat {print $5}' | 
   cut -d: -f1 | sort | uniq -c | sort | tail -n5 | 
   grep -oP '(\d+\.){3}\d+' | 
