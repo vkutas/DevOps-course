e06291356696abff2f16262c3ae075eeb80d09e1
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-20 12:04:10 +0300

    Refractoring of while loop

--- task1/whose.sh
+++ task1/whose.sh
@@ -3,4 +3,7 @@ PROCESS=$1
 sudo netstat -tunapl | awk -v pat="$PROCESS" '$7~pat {print $5}' | 
   cut -d: -f1 | sort | uniq -c | sort | tail -n5 | 
   grep -oP '(\d+\.){3}\d+' | 
-  while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+  while read IP
+   do 
+   	whois $IP | awk 'BEGIN {FS = ":"}; /^Organization/ {print $2}';
+   done
