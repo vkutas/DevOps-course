3efbed0e4fb9442a70166ef1bb1059633ce3d29d
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-20 11:31:40 +0300

    Some refractoring was made to make script more redable.

--- task1/whose.sh
+++ task1/whose.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-sudo netstat -tunapl | awk '/firefox/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+sudo netstat -tunapl | awk '/firefox/ {print $5}' | 
+  cut -d: -f1 | sort | uniq -c | sort | tail -n5 | 
+  grep -oP '(\d+\.){3}\d+' | 
+  while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
