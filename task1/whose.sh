e025d16a9ef8e36374f0364bbd5598652da4f3d5
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-20 11:38:17 +0300

    Added paramitrization for process. Now, script take process's name or PID as input.

--- task1/whose.sh
+++ task1/whose.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo netstat -tunapl | awk '/firefox/ {print $5}' | 
+PROCESS=$1
+sudo netstat -tunapl | awk -v pat="$PROCESS" '$7~pat {print $5}' | 
   cut -d: -f1 | sort | uniq -c | sort | tail -n5 | 
   grep -oP '(\d+\.){3}\d+' | 
   while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
