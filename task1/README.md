ee266d6d2182b3b56496742f10092b696a4e5efa
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-20 16:28:30 +0300

    Added parametrization in order to get other conncetion states. README.md updated accordingly.

--- task1/README.md
+++ task1/README.md
@@ -1,10 +1,14 @@
-This script finds active network connections of passed procces (process name or PID) and for each Foreing Address print information about IP owner. 
-By default output only contains the name of the organization to which the IP belongs. In order to get more information use -v, -vv or -vvv. 
+This script finds network connections procces (process name or PID) and for each Foreing Address print information about IP owner. 
+By default its looking for UDP connections and TCP connections of all states and return the name of the Organization to which the Remote Address belongs.
+
+In order to get more information about remote IP address owner, options -v, -vv or -vvv could be used as decribe below.
+In order to get information about other connections states, options -e, -eu, -a could be used as decribe below.
 
 USAGE
- task_1.sh process_name [OPTIONS]
-	OR
- task_1.sh PID	[OPTIONS]
+ task_1.sh [OPTIONS] process_name 
+ task_1.sh [OPTIONS] PID
+ task_1.sh process_name [OPTIONS]  
+ task_1.sh PID [OPTIONS]
 
  OPTIONS
  -v  
@@ -15,3 +19,12 @@ USAGE
 
  -vvv 
  Print inforamtion about country,city, address and postal code where IP owner located
+
+-e
+Show information only about TCP connections in ESTABLISHED state.
+
+-eu
+Show information about TCP connections in ESTABLISHED state and all UDP connections.
+
+-a
+Show information only about TCP connections in all states.
--- task1/whose.sh
+++ task1/whose.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
-if [ ! -n "$1" ]
+while [ -n "$1" ]
+ do
+ 	case "$1" in
+
+ 			-v    ) OUTPUT_DATA='^Organization|^Country';;
+			-vv   ) OUTPUT_DATA='^Organization|^Country|^City';;
+			-vvv  ) OUTPUT_DATA='^Organization|^Country|^City|^Addres|^PostalCode';;
+ 			-e    ) CONNECTIONS_DETAILS=$(sudo netstat -tnp | grep -w 'ESTABLISHED');;
+            -eu   ) CONNECTIONS_DETAILS=$(sudo netstat -tunp | awk '$6 =="ESTABLISHED" || $6 == "/$^/" {print $0}');;
+ 			-a    ) CONNECTIONS_DETAILS=$(sudo netstat -tnap);;
+			*     ) PROCESS=$1;;
+ 		esac
+	shift;			
+done
+
+if [ ! -n "$PROCESS" ] 
  then 
- 	echo "Process name or PID must be specified";
+   	echo "Process name or PID must be specified";
  	echo "Example of usage: 'whose.sh firefox' or 'whose.sh 1287'";
  	exit 1;
 fi 	
-case "$2" in
-	-v)   OUTPUT_PATTERN='Organization|^Country';;
-	-vv)  OUTPUT_PATTERN='Organization|^Country|^City';;
-    -vvv) OUTPUT_PATTERN='Organization|^Country|^City|^Addres|^PostalCode';;
-    *)    OUTPUT_PATTERN='^Organization';;
-esac
-  PROCESS=$1
-sudo netstat -tunapl | awk -v pat="$PROCESS" '$7~pat {print $5}' | 
+
+if [ ! -n "$CONNECTIONS_DETAILS" ] 
+ then CONNECTIONS_DETAILS=$(sudo netstat -tunap)
+fi
+
+if [ ! -n "$OUTPUT_DATA" ] 
+ then OUTPUT_DATA='^Organization'
+fi
+
+echo "$CONNECTIONS_DETAILS" | awk -v pat="$PROCESS" '$7~pat {print $5}' | 
   cut -d: -f1 | sort | uniq -c | sort | tail -n5 | 
   grep -oP '(\d+\.){3}\d+' | 
   while read IP
    do 
    	whois $IP | 
-   	awk -v pat="$OUTPUT_PATTERN" ' 	     
+   	awk -v pat="$OUTPUT_DATA" ' 	     
    		BEGIN {FS = ":"; patt = $PATTERN;}; 
    		$1 ~ pat {print $1, $2}';
     echo '\n';
