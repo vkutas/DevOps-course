79e54f23f17297ebe59d741f6e6aa57e548f9b67
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-24 15:39:35 +0300

    Refractoring

--- task1/whose.sh
+++ task1/whose.sh
@@ -1,61 +1,60 @@
-#!/bin/sh
 export PATH="/usr/local/bin:/usr/bin";
 EXAMPLE_OF_USAGE="Example of usage: 'whose_v2.sh firefox' or 'whose_v2.sh 1287'. \nSee README_v2.md for more details.\n";
 PROCESS="$1";
 shift;
 
 while [ -n "${1}" ]; do
- 	case "$1" in
-
- 			-v    ) OUTPUT_DATA='/Organization\|Country/p'; n=2;;
-			-vv   ) OUTPUT_DATA='/Organization\|Country\|City/p'; n=3;;
-			-vvv  ) OUTPUT_DATA='/Organization\|Country\|City\|Address\|PostalCode/p';;
- 			-e    ) CONNECTIONS_DETAILS=$(netstat -tnp | grep -w 'ESTABLISHED');;
-      -eu   ) CONNECTIONS_DETAILS=$(netstat -tunp | awk '$6 =="ESTABLISHED" || $1 == "udp" {print $0}');;
- 			-a    ) CONNECTIONS_DETAILS=$(netstat -tnap);;
-			-n    ) NUMBER_OF_CONNECTIONS="$2";
-					    shift;
-              ;;
-			*     ) echo "Option ${1} is unknown";
-              echo "$EXAMPLE_OF_USAGE";
-              exit 1;
-              ;;
- 		esac
-	shift;			
+    case "$1" in
+        
+        -v    ) OUTPUT_DATA='/Organization\|Country/p'; n=2;;
+        -vv   ) OUTPUT_DATA='/Organization\|Country\|City/p'; n=3;;
+        -vvv  ) OUTPUT_DATA='/Organization\|Country\|City\|Address\|PostalCode/p';;
+        -e    ) CONNECTIONS_DETAILS=$(netstat -tnp | grep -w 'ESTABLISHED');;
+        -eu   ) CONNECTIONS_DETAILS=$(netstat -tunp | awk '$6 =="ESTABLISHED" || $1 == "udp" { print $0 }');;
+        -a    ) CONNECTIONS_DETAILS=$(netstat -tnap);;
+        -n    ) NUMBER_OF_CONNECTIONS="$2";
+            shift;
+        ;;
+        *     ) echo "Option ${1} is unknown";
+            echo "$EXAMPLE_OF_USAGE";
+            exit 1;
+        ;;
+    esac
+    shift;
 done
 
-if [ ! -n "${PROCESS}" ]; then 
-   	printf "Process name or PID must be specified\n${EXAMPLE_OF_USAGE}";
- 	  printf "$EXAMPLE_OF_USAGE";
- 	exit 1;
-fi 	
-
-if [ ! -n "${CONNECTIONS_DETAILS}" ]; then 
-	CONNECTIONS_DETAILS=$(netstat -tunap)
+if [ ! -n "${PROCESS}" ]; then
+    printf "Process name or PID must be specified\n${EXAMPLE_OF_USAGE}";
+    printf "$EXAMPLE_OF_USAGE";
+    exit 1;
 fi
 
-if [ ! -n "${OUTPUT_DATA}" ]; then 
-	OUTPUT_DATA='/Organization/p'
+if [ ! -n "${CONNECTIONS_DETAILS}" ]; then
+    CONNECTIONS_DETAILS=$(netstat -tunap)
 fi
 
-if [ ! -n "${NUMBER_OF_CONNECTIONS}" ]; then 
-	NUMBER_OF_CONNECTIONS=5
+if [ ! -n "${OUTPUT_DATA}" ]; then
+    OUTPUT_DATA='/Organization/p'
 fi
 
-PROCESS_CONNECTIONS=$(echo "${CONNECTIONS_DETAILS}" | awk -v pat="$PROCESS" '$7~pat {print $5}');
-if [ ! -n "${PROCESS_CONNECTIONS}" ]; then 
-	echo "Connections for process \"${PROCESS}\" not found.";
-  echo "$EXAMPLE_OF_USAGE";
-	exit 1;
+if [ ! -n "${NUMBER_OF_CONNECTIONS}" ]; then
+    NUMBER_OF_CONNECTIONS=5
 fi
 
-echo "${PROCESS_CONNECTIONS}" | cut -d: -f1 | sort | uniq -c | sort | tail -n"${NUMBER_OF_CONNECTIONS}" | 
-  while read ADDRES_LINE; do 
-   	CON_PER_IP=$(echo "$ADDRES_LINE" | cut -d' ' -f1);
-   	IP=$(echo "$ADDRES_LINE" | cut -d' ' -f2);
-   	DATA=$(whois "$IP" | sed -n $OUTPUT_DATA | tail -"$n");
-   	echo "Remote IP Address: ${IP}";
-   	echo "Number of Connections ${CON_PER_IP}"
+PROCESS_CONNECTIONS=$(echo "${CONNECTIONS_DETAILS}" | awk -v pat="$PROCESS" '$7~pat { print $5 }');
+if [ ! -n "${PROCESS_CONNECTIONS}" ]; then
+    echo "Connections for process \"${PROCESS}\" not found.";
+    echo "$EXAMPLE_OF_USAGE";
+    exit 1;
+fi
+
+echo "${PROCESS_CONNECTIONS}" | cut -d: -f1 | sort | uniq -c | sort | tail -n"${NUMBER_OF_CONNECTIONS}" |
+while read ADDRES_LINE; do
+    CON_PER_IP=$(echo "$ADDRES_LINE" | cut -d' ' -f1);
+    IP=$(echo "$ADDRES_LINE" | cut -d' ' -f2);
+    DATA=$(whois "$IP" | sed -n $OUTPUT_DATA | tail -"$n");
+    echo "Remote IP Address: ${IP}";
+    echo "Number of Connections ${CON_PER_IP}"
     echo "$DATA";
     echo;
-   done
+done
--- task1/whose_v2.sh
+++ task1/whose_v2.sh
@@ -5,95 +5,95 @@ PROCESS="$1";
 shift;
 CL_FLAGS="$*";
 while [ -n "${1}" ]; do
- 	case "$1" in
- 			-e    ) CONNECTIONS_DETAILS=$(netstat -tnp | grep -w 'ESTABLISHED');;
-      -u    ) CONNECTIONS_DETAILS=$(netstat -tunp | awk '$6 =="ESTABLISHED" || $1 == "udp" {print $0}');; 
- 			-a    ) CONNECTIONS_DETAILS=$(netstat -tnap);;
-			-n    ) NUMBER_OF_CONNECTIONS="$2";
-			        shift;;
-			*     ) ;;
- 		esac
-	shift;			
+    case "$1" in
+        -e    ) CONNECTIONS_DETAILS=$(netstat -tnp | grep -w 'ESTABLISHED');;
+        -u    ) CONNECTIONS_DETAILS=$(netstat -tunp | awk '$6 =="ESTABLISHED" || $1 == "udp" { print $0 }');;
+        -a    ) CONNECTIONS_DETAILS=$(netstat -tnap);;
+        -n    ) NUMBER_OF_CONNECTIONS="$2";
+        shift;;
+        *     ) ;;
+    esac
+    shift;
 done
 
-if [ -z "${PROCESS}" ]; then 
-  printf "Process name or PID must be specified\n %s" "${EXAMPLE_OF_USAGE}";
- 	exit 1;
-fi 	
-
-if [ -z "${CONNECTIONS_DETAILS}" ]; then 
-	CONNECTIONS_DETAILS=$(netstat -tunap)
+if [ -z "${PROCESS}" ]; then
+    printf "Process name or PID must be specified\n %s" "${EXAMPLE_OF_USAGE}";
+    exit 1;
 fi
 
-if [ -z "${OUTPUT_DATA}" ]; then 
-	OUTPUT_DATA='^Organization'
+if [ -z "${CONNECTIONS_DETAILS}" ]; then
+    CONNECTIONS_DETAILS=$(netstat -tunap)
 fi
 
-if [ -z "${NUMBER_OF_CONNECTIONS}" ]; then 
-	NUMBER_OF_CONNECTIONS=5
+if [ -z "${OUTPUT_DATA}" ]; then
+    OUTPUT_DATA='^Organization'
 fi
 
-PROCESS_CONNECTIONS=$(echo "${CONNECTIONS_DETAILS}" | awk -v pat="$PROCESS" '$7~pat {print $5}');
-if [ -z "${PROCESS_CONNECTIONS}" ]; then 
-	printf "Connections for process \"%s\" not found." "${PROCESS}";
-	exit 1;
+if [ -z "${NUMBER_OF_CONNECTIONS}" ]; then
+    NUMBER_OF_CONNECTIONS=5
+fi
+
+PROCESS_CONNECTIONS=$(echo "${CONNECTIONS_DETAILS}" | awk -v pat="$PROCESS" '$7~pat { print $5 }');
+if [ -z "${PROCESS_CONNECTIONS}" ]; then
+    printf "Connections for process \"%s\" not found." "${PROCESS}";
+    exit 1;
 fi
 
 LINES=$(echo "${PROCESS_CONNECTIONS}" | cut -d: -f1 | sort | uniq -c | sort | tail -n"${NUMBER_OF_CONNECTIONS}");
-  while read -r LINE; do 
-   	CON_PER_IP=$(echo "$LINE" | cut -d' ' -f1);
-   	IP=$(echo "$LINE" | cut -d' ' -f2);
-   	DATA=$(whois "$IP");
-   	ORGANIZATION=$(echo "$DATA" | sed -n '/^OrgName:\|^org-name:/p' | tail -1 | sed 's/^OrgName:\s*//g; s/^org-name:\s*//g'); 
+while read -r LINE; do
+    CON_PER_IP=$(echo "$LINE" | cut -d' ' -f1);
+    IP=$(echo "$LINE" | cut -d' ' -f2);
+    DATA=$(whois "$IP");
+    ORGANIZATION=$(echo "$DATA" | sed -n '/^OrgName:\|^org-name:/p' | tail -1 | sed 's/^OrgName:\s*//g; s/^org-name:\s*//g');
     if [ -z "${ORGANIZATION}" ]; then
-          ORGANIZATION="(Not Found)";
-    fi  
+        ORGANIZATION="(Not Found)";
+    fi
     OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "Remote IP:" "${IP}");
     OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "Count of Connections:" "${CON_PER_IP}");
     OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "Organization:" "${ORGANIZATION}");
-
-# If 'v' is pasted as option, add Country and City to the output.    
-  	if test "$(echo "$CL_FLAGS "| grep 'v')"; then
-      	COUNTRY=$(echo "$DATA" | sed -n '/^Country\|^country:/p' | tail -1 | sed 's/^Country:\s*//g; s/^country:\s*//g');
+    
+    # If 'v' is pasted as option, add Country and City to the output.
+    if test "$(echo "$CL_FLAGS "| grep 'v')"; then
+        COUNTRY=$(echo "$DATA" | sed -n '/^Country\|^country:/p' | tail -1 | sed 's/^Country:\s*//g; s/^country:\s*//g');
         if [ -z "${COUNTRY}" ]; then
-      	  COUNTRY="(Not Found)";
-      	fi	
+            COUNTRY="(Not Found)";
+        fi
         OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "Country:" "${COUNTRY}");
         
         CITY=$(echo "$DATA" | sed -n '/^City:\|^city:/p' | tail -1 | sed 's/^City:\s*//g; s/^city:\s*//g' );
         if [ -z "${CITY}" ]; then
-          CITY="(Not Found)";
+            CITY="(Not Found)";
         fi
-        OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "City:" "${CITY}");      
-      fi
-
-# If 'vv' is passed as option, add Address and Postal Code to the output.
-    if test "$(echo "$CL_FLAGS "| grep 'vv')"; then
-      	ADDRESS=$(echo "$DATA" | sed -n '/^Address:\|^address:/p' | tail -2 | sed 's/^Address:\s*//g; s/^address:\s*//g');
-# If Address contains two or more lines, concatenate them
-      	if [ "$(echo "$ADDRESS" | wc -l)" -ge 2 ]; then 
-      		ADDRESS="$(echo "$ADDRESS" | paste -sd ',')";
-      	fi
-      	if [ -z "${ADDRESS}" ]; then
-      	  ADDRESS="(Not Found)";
-      	fi
-        OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "Address:" "${ADDRESS}");
-
-      	POSTAL_CODE=$(echo "$DATA" | sed -n '/^PostalCode:\|^postal-code:/p' | tail -1 | sed 's/^PostalCode:\s*//g; s/^postal-code:\s*//g');
-      	if [ -z "${POSTAL_CODE}" ]; then
-      	  POSTAL_CODE="(Not Found)";
-      	fi
-      	OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "PostalCode:" "${POSTAL_CODE}");
+        OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "City:" "${CITY}");
     fi
-
+    
+    # If 'vv' is passed as option, add Address and Postal Code to the output.
+    if test "$(echo "$CL_FLAGS "| grep 'vv')"; then
+        ADDRESS=$(echo "$DATA" | sed -n '/^Address:\|^address:/p' | tail -2 | sed 's/^Address:\s*//g; s/^address:\s*//g');
+        # If Address contains two or more lines, concatenate them
+        if [ "$(echo "$ADDRESS" | wc -l)" -ge 2 ]; then
+            ADDRESS="$(echo "$ADDRESS" | paste -sd ',')";
+        fi
+        if [ -z "${ADDRESS}" ]; then
+            ADDRESS="(Not Found)";
+        fi
+        OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "Address:" "${ADDRESS}");
+        
+        POSTAL_CODE=$(echo "$DATA" | sed -n '/^PostalCode:\|^postal-code:/p' | tail -1 | sed 's/^PostalCode:\s*//g; s/^postal-code:\s*//g');
+        if [ -z "${POSTAL_CODE}" ]; then
+            POSTAL_CODE="(Not Found)";
+        fi
+        OUTPUT=$(printf "%s\n%-21s %s" "${OUTPUT}" "PostalCode:" "${POSTAL_CODE}");
+    fi
+    
     OUTPUT=$(printf "%s\n\r" "${OUTPUT}");
-   done < <(echo "${LINES}")
+done < <(echo "${LINES}")
 
-   printf "%s" "${OUTPUT}"; 
+printf "%s" "${OUTPUT}";
 
 if test "$(echo "$CL_FLAGS" | grep 'c')"; then
-  printf "\n#Connections per Organization#\n"
-  echo "$OUTPUT" | sed -n '/^Organization:/p' | sed 's/^Organization:\s*//g' | sort | uniq -c | sort | 
-  awk 'BEGIN {printf "%-20s %s\n", "Count of connections", "Organization" }
+    printf "\n#Connections per Organization#\n"
+    echo "$OUTPUT" | sed -n '/^Organization:/p' | sed 's/^Organization:\s*//g' | sort | uniq -c | sort |
+    awk 'BEGIN {printf "%-20s %s\n", "Count of connections", "Organization" }
     { printf "%-20s %s\n", $1, substr($0, index($0, $2)) }';
-fi 
+fi
