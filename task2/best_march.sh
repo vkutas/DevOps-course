6528d57671c84dcea77dea175061b47b8ab87d4a
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-02-27 20:45:13 +0300

    Change varible name to be more self explainable.

--- task2/best_march.sh
+++ task2/best_march.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-LINES=$(jq '.prices[] | [. | .[0], ((.[0] / 1000) | strftime("%m")), ((.[0] / 1000) | strftime("%y")), .[1]] | select(.[1] == "03")' quotes.json);
+RATES=$(jq '.prices[] | [. | .[0], ((.[0] / 1000) | strftime("%m")), ((.[0] / 1000) | strftime("%y")), .[1]] | select(.[1] == "03")' quotes.json);
 
 for YEAR in $(seq 15 20); do 
-    YEAR_VAL=$(echo "$LINES" | jq --arg year "$YEAR" '. | select(.[2] == $year) | .[3]' | sort -g)
+    YEAR_VAL=$(echo "$RATES" | jq --arg year "$YEAR" '. | select(.[2] == $year) | .[3]' | sort -g)
     YEAR_MIN=$(echo "$YEAR_VAL" | head -n1);
     YEAR_MAX=$(echo "$YEAR_VAL" | tail -n1);
     VOLATILITY=$(printf "%s\n\r20%s %s" "$VOLATILITY" "$YEAR" $(echo "scale=4; ($YEAR_MAX - $YEAR_MIN) / 2" | bc))
