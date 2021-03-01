c6081ce9b635b0ca34f11491733d7cf204868ad6
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-03-01 16:08:25 +0300

    Count of PRs with labels for each contributor added to the output (part 3 of task 3).

--- task3/git_get.sh
+++ task3/git_get.sh
@@ -10,13 +10,10 @@ url=$1;
 repo_name=$(echo "$url" | cut -d'/' -f5);
 user_name=$(echo "$url" | cut -d'/' -f4);
 
-echo "DEBUG: $repo_name"
-echo "DEBUG: $user_name"
-
 # Init resulting varible with empty json array
 payload="[]";
 
-printf "Geting data from the repo '%s' of user '%s'..." "$repo_name" "$user_name"
+printf "Geting data from the repo '%s' of user '%s'...\n\r" "$repo_name" "$user_name"
 
 # curl open PR page by page, 100 per query.
 for ((i=1; ; i++)); do
@@ -28,11 +25,11 @@ for ((i=1; ; i++)); do
 
     # If response code is not 200, then print error message and exit the programm
     if [[ "$response_code" -ne 200 ]]; then 
-        printf "Error: http response code is %s\n\r" "$response_code"
+        printf "Error: http response code %s\n\r" "$response_code"
         exit 1;
     fi
     # Extract the payload and remove unnecessary staff to make it more compact
-    data=$(echo "$response" | head -n -1 | jq -c '[.[] | { u: .user | .login, l: [.labels | map(.name)]}]');
+    data=$(echo "$response" | head -n -1 | jq -c '[.[] | { u: .user | .login, l: .labels | map(.name)}]');
 
     # If response body contains an empty array, then exit the loop
     if [[ $(echo "$data" | jq '. | length') == 0 ]]; then
@@ -45,7 +42,25 @@ done
 
 echo "$payload" > test.json;
 
-printf "Found %s opened pull requests.\n\r" "$(echo "$payload" | jq '. | length')"
+printf "...\n\rFound %s opened pull requests.\n\r" "$(echo "$payload" | jq '. | length')"
 
-printf "Most productive contributors:\n\r";
-echo "$payload" | jq '.[].u' | sort | uniq -c | sort -gr -k 1 | awk 'BEGIN {printf "\tUser name\t\tOpen Pull Requests\n"}; $1 > 1 {printf "%-17s\t%16s\n", $2, $1}'
+printf "\n\r%34s\n\r" "Most productive contributors";
+printf "#%.0s" {1..41}
+printf "\n\r%-12s\t%20s\n\r" "USER" "COUNT OF PRs"
+printf "=%.0s" {1..41}
+echo "$payload" | jq '.[].u' | sort | uniq -c | sort -gr -k 1 | awk '$1 > 1 {printf "\n\r%-17s\t%16s", $2, $1}'
+
+
+contributors=$(echo "$payload" | jq -rc 'group_by(.u) |  .[] | {c: . | length, usr: .[0].u, lbls: . | map(.l[]) | unique }')
+
+printf "\n\r%35s\n\r" "Open Pull Requests";
+printf "#%.0s" {1..52}
+printf "\n\r%-12s\t%12s\t%20s\n\r" "USER" "COUNT OF PRs" "LABELS"
+printf "=%.0s" {1..52};
+while IFS= read -r contributor; do
+    printf "\n\r%-12s\t%6s\t\t%-28s" \
+        "$(echo "$contributor" | jq -r '.usr')" \
+        "$(echo "$contributor" | jq -r '.c')" \
+        "$(echo "$contributor" | jq -r '.lbls | join(", ")')";
+done <<< "$contributors"
+echo
